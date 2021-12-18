efd16822cefb544aad54a3e4d27e5217adfb2acf
tmjo <54450177+tmjo@users.noreply.github.com>
2021-12-18 01:43:33 +0100

    Update localize.js

--- src/localize.js
+++ src/localize.js
@@ -27,8 +27,9 @@ export default function localize(string, search = '', replace = ''){
   } catch (e) {
     try {
       translated = string.split('.').reduce((o, i) => o[i], languages['en']);
-    } catch (ee) {
-      translated = string;
+    }
+    catch (ee) {
+      translated = string.split('.')[2];
     }
   }
 
@@ -38,46 +39,5 @@ export default function localize(string, search = '', replace = ''){
   if (search !== '' && replace !== '') {
     translated = translated.replace(search, replace);
   }
-  return translated || string;
+  return translated || string.split('.')[1];
 }
-
-
-// const DEFAULT_LANG = 'en';
-
-// export default function localize(string, search, replace) {
-//   const [section, key] = string.split('.');
-
-//   let langStored;
-
-//   try {
-//     langStored = JSON.parse(localStorage.getItem('selectedLanguage'));
-//   } catch (e) {
-//     langStored = localStorage.getItem('selectedLanguage');
-//   }
-
-//   const lang = (langStored || navigator.language.split('-')[0] || DEFAULT_LANG)
-//     .replace(/['"]+/g, '')
-//     .replace('-', '_');
-
-//   let translated;
-
-//   try {
-//     translated = languages[lang][section][key];
-//   } catch (e) {
-//     translated = languages[DEFAULT_LANG][section][key];
-//   }
-
-//   if (translated === undefined) {
-//     translated = languages[DEFAULT_LANG][section][key];
-//   }
-
-//   if (translated === undefined) {
-//     return string;
-//   }
-
-//   if (search !== '' && replace !== '') {
-//     translated = translated.replace(search, replace);
-//   }
-
-//   return translated;
-// }
