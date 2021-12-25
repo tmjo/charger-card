d9aa8ba5bf3894670a5e818ebd07b4514f00bd5b
tmjo <54450177+tmjo@users.noreply.github.com>
2021-12-25 23:32:42 +0100

    Update localize.js

--- src/localize.js
+++ src/localize.js
@@ -17,27 +17,47 @@ var languages = {
   ca
 };
 
-export default function localize(string, search = '', replace = ''){
+export default function localize(string, brand=null, search = '', replace = '', debug=false){
   const lang = (localStorage.getItem('selectedLanguage') || 'en').replace(/['"]+/g, '').replace('-', '_');
-
   let translated;
+  let brandstr = brand === undefined || brand === null ? string : brand + "." + string;
 
   try {
-    translated = string.split('.').reduce((o, i) => o[i], languages[lang]);
-  } catch (e) {
-    try {
-      translated = string.split('.').reduce((o, i) => o[i], languages['en']);
+    // Try to translate, add brand if valid
+    translated = brandstr.split('.').reduce((o, i) => o[i], languages[lang]);
+    if(debug) console.log("Translating 1: " + string + " --> " + brandstr + " --> " + translated);
+
+    if (translated === undefined) {
+      translated = brandstr.toLowerCase().split('.').reduce((o, i) => o[i], languages[lang]);
+      if(debug) console.log("Translating 2: " +string +" --> " +brandstr +" --> " +translated);
     }
-    catch (ee) {
-      translated = string.split('.')[2];
+
+    if (translated === undefined) {
+      translated = brandstr.split('.').reduce((o, i) => o[i], languages['en']);
+      if(debug) console.log("Translating 3: " +string +" --> " +brandstr +" --> " +translated);
     }
+
+    if (translated === undefined) {
+      translated = brandstr.toLowerCase().split('.').reduce((o, i) => o[i], languages['en']);
+      if(debug) console.log("Translating 4: " +string +" --> " +brandstr +" --> " +translated);
+    }
+  }catch (e) {
+    // Give up, do nothing
   }
 
-  if (translated === undefined) translated = string.split('.').reduce((o, i) => o[i], languages['en']);
-  // if (translated === undefined) translated = string;
 
+  if (translated === undefined) {
+    // If translation failed, return last item of array
+    var temp = string.split(".");
+    translated = temp.length > 0 ? temp[temp.length-1] : temp;
+    if(debug) console.log("Gave up translating: " +string +" --> " +temp +" --> " +translated);
+  }
+
+  //Search and replace
   if (search !== '' && replace !== '') {
     translated = translated.replace(search, replace);
   }
-  return translated || string.split('.')[1];
+
+  //Return
+  return translated || string;
 }
